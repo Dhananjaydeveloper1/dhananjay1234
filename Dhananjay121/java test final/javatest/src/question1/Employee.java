dda399ec5564bc2f0fdbc714978fcdcd279ba3d5
Dhananjay Kumar <dhananjay.kumar@espire.com>
2021-10-11 18:03:18 +0530

    :microservice

---
+++ Dhananjay121/java test final/javatest/src/question1/Employee.java
@@ -0,0 +1,29 @@
+package question1;
+//Create an ArrayList of Employee( id,name,address,sal) objects and retrieve objects from ArrayList by using forEach() method of Iterable interface.
+import java.util.Scanner;
+public class Employee {
+	private String id = null;
+	private String name = null;
+	private String addrs = null;
+	private int sal = 0;
+	
+	void getData() {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Employee ID:  ");
+		id = sc.next();
+		System.out.println("Name:         ");
+		name = sc.next();
+		System.out.println("Address:      ");
+		addrs = sc.next();
+		System.out.println("Salary:       ");
+		sal = sc.nextInt();
+	}
+	
+	void displayData() {
+		System.out.println("Employee ID:  "+id);
+		System.out.println("Name:         "+name);
+		System.out.println("Address:      "+addrs);
+		System.out.println("Salary:       "+sal);
+	}
+}
